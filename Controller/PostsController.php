8e5d7b9187e8f130c67b39ac99a4435e43c7d2b2
Mirko Pagliai <mirko.pagliai@gmail.com>
2014-08-24 18:27:22 +0200

    fixed descriptions

--- Controller/PostsController.php
+++ Controller/PostsController.php
@@ -4,26 +4,26 @@ App::uses('MeCmsAppController', 'MeCms.Controller');
 /**
  * PostsController
  *
- * This file is part of MeCms.
+ * This file is part of MeCms Backend
  *
- * MeCms is free software: you can redistribute it and/or modify
+ * MeCms Backend is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
  *
- * MeCms is distributed in the hope that it will be useful,
+ * MeCms Backend is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with MeCms.  If not, see <http://www.gnu.org/licenses/>.
+ * along with MeCms Backend.  If not, see <http://www.gnu.org/licenses/>.
  *
  * @author		Mirko Pagliai <mirko.pagliai@gmail.com>
  * @copyright	Copyright (c) 2014, Mirko Pagliai for Nova Atlantis Ltd
  * @license		http://www.gnu.org/licenses/agpl.txt AGPL License
  * @link		http://git.novatlantis.it Nova Atlantis Ltd
- * @package		MeCms\Controller
+ * @package		MeCmsBackend\Controller
  */
 
 /**
