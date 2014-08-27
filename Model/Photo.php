bcf2a4913a68d183d06e74ba37d0f21415c7383a
Mirko Pagliai <mirko.pagliai@gmail.com>
2014-08-27 15:47:38 +0200

    added Photo and PhotosAlbum models

---
+++ Model/Photo.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Photo
+ *
+ * This file is part of MeCms Backend.
+ *
+ * MeCms Backend is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * MeCms Backend is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with MeCms Backend.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author		Mirko Pagliai <mirko.pagliai@gmail.com>
+ * @copyright	Copyright (c) 2014, Mirko Pagliai for Nova Atlantis Ltd
+ * @license		http://www.gnu.org/licenses/agpl.txt AGPL License
+ * @link		http://git.novatlantis.it Nova Atlantis Ltd
+ * @package		MeCmsBackend\Model
+ */
+
+App::uses('MeCmsBackendAppModel', 'MeCmsBackend.Model');
+
+/**
+ * Photo Model
+ * @property Album $Album
+ */
+class Photo extends MeCmsBackendAppModel {
+	/**
+	 * Display field
+	 * @var string
+	 */
+	public $displayField = 'filename';
+
+	/**
+	 * Validation rules
+	 * @var array
+	 */
+	public $validate = array(
+		'id' => array(
+			//Blank on create
+			'on'	=> 'create',
+			'rule'	=> 'blank'
+		),
+		'album_id' => array(
+			'message'	=> 'You have to select an option',
+			'rule'		=> array('naturalNumber')
+		),
+		'filename' => array(
+			'message'	=> 'This extension is invalid',
+			'rule'		=> array('extension', array('gif', 'jpg', 'jpeg', 'png'))
+		),
+		'description' => array(
+			'allowEmpty'	=> TRUE,
+			'message'		=> 'Must be at most %d chars',
+			'rule'			=> array('maxLength', 255)
+		)
+	);
+
+	/**
+	 * belongsTo associations
+	 * @var array
+	 */
+	public $belongsTo = array(
+		'Album' => array(
+			'className' => 'Album',
+			'foreignKey' => 'album_id'
+		)
+	);
+}
---
+++ Model/PhotosAlbum.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * PhotosAlbum
+ *
+ * This file is part of MeCms Backend.
+ *
+ * MeCms Backend is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * MeCms Backend is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with MeCms Backend.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author		Mirko Pagliai <mirko.pagliai@gmail.com>
+ * @copyright	Copyright (c) 2014, Mirko Pagliai for Nova Atlantis Ltd
+ * @license		http://www.gnu.org/licenses/agpl.txt AGPL License
+ * @link		http://git.novatlantis.it Nova Atlantis Ltd
+ * @package		MeCmsBackend\Model
+ */
+
+App::uses('MeCmsBackendAppModel', 'MeCmsBackend.Model');
+
+/**
+ * PhotosAlbum Model
+ * @property Photo $Photo
+ */
+class PhotosAlbum extends MeCmsBackendAppModel {
+	/**
+	 * Display field
+	 * @var string
+	 */
+	public $displayField = 'title';
+
+	/**
+	 * Validation rules
+	 * @var array
+	 */
+	public $validate = array(
+		'id' => array(
+			//Blank on create
+			'on'	=> 'create',
+			'rule'	=> 'blank'
+		),
+		'title' => array(
+			'between' => array(
+				'last'		=> FALSE,
+				'message'	=> 'Must be between %d and %d chars',
+				'rule'		=> array('between', 3, 100)
+			),
+			'isUnique' => array(
+				'message'	=> 'This value is already used',
+				'rule'		=> 'isUnique'
+			)
+		),
+		'slug' => array(
+			'slug' => array(
+				'last'		=> FALSE,
+				'message'	=> 'Allowed chars: lowercase letters, numbers, dash',
+				'rule'		=> array('custom', '/^[a-z0-9\-]+$/')
+			),
+			'between' => array(
+				'last'		=> FALSE,
+				'message'	=> 'Must be between %d and %d chars',
+				'rule'		=> array('between', 3, 100)
+			),
+			'isUnique' => array(
+				'message'	=> 'This value is already used',
+				'rule'		=> 'isUnique'
+			)
+		),
+		'description' => array(
+			'allowEmpty'	=> TRUE,
+			'message'		=> 'Must be at most %d chars',
+			'rule'			=> array('maxLength', 255)
+		)
+	);
+
+	/**
+	 * hasMany associations
+	 * @var array
+	 */
+	public $hasMany = array(
+		'Photo' => array(
+			'className' => 'Photo',
+			'foreignKey' => 'album_id',
+			'dependent' => FALSE
+		)
+	);
+
+}
