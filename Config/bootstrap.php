d24b02ae95f358d3050c36162b63ad3291d205ee
Mirko Pagliai <mirko.pagliai@gmail.com>
2014-09-21 12:52:42 +0200

    added options for Memcache engine and file description

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -1,9 +1,35 @@
 <?php
+/**
+ * Bootstrap.
+ *
+ * This file is part of MeCms.
+ *
+ * MeCms is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * MeCms is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with MeCms.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author		Mirko Pagliai <mirko.pagliai@gmail.com>
+ * @copyright	Copyright (c) 2014, Mirko Pagliai for Nova Atlantis Ltd
+ * @license		http://www.gnu.org/licenses/agpl.txt AGPL License
+ * @link		http://git.novatlantis.it Nova Atlantis Ltd
+ * @package		MeCms\Config
+ */
 
 //Adds the admin routes prefix
 Configure::write('Routing.prefixes', array('admin'));
 
-//Default options
+/**
+ * Default options (with File engine)
+ */
 $options = array(
 	'duration'	=> '+999 days',
 	'engine'	=> 'File',
@@ -11,6 +37,15 @@ $options = array(
 	'path'		=> CACHE.'mecms'.DS,
 	'prefix'	=> NULL
 );
+
+/**
+ * Default options (with Memcache engine)
+ */
+//$options = array(
+//	'duration'	=> '+999 days',
+//	'engine'	=> 'Memcache',
+//	'prefix'	=> Inflector::slug(APP_DIR).'_'
+//);
 
 Cache::config('pages', am($options, array('groups' => array('pages'))));
 Cache::config('photos', am($options, array('groups' => array('photos'))));
