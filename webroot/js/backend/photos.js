eedf5664321ebd02844ce285fdd6d105744cb9b0
Mirko Pagliai <mirko.pagliai@gmail.com>
2014-10-18 08:06:19 +0200

    fixed comments

--- webroot/js/backend/photos.js
+++ webroot/js/backend/photos.js
@@ -1,31 +1,13 @@
-/**
- * Photos js.
- *
+/*!
  * This file is part of MeCms.
- *
- * MeCms is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * MeCms is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with MeCms.  If not, see <http://www.gnu.org/licenses/>.
- *
  * @author		Mirko Pagliai <mirko.pagliai@gmail.com>
  * @copyright	Copyright (c) 2014, Mirko Pagliai for Nova Atlantis Ltd
  * @license		http://www.gnu.org/licenses/agpl.txt AGPL License
- * @link		http://git.novatlantis.it Nova Atlantis Ltd
- * @package		MeCms\webroot\js
  */
 
 $(function() {
 	/**
-	 * When you click on "check/uncheck all", it changes the checkboxed state
+	 * When adding photos, on click on "check/uncheck all", it changes the checkboxed state
 	 */
 	$('.photos .check-all, .photos .uncheck-all').click(function(event) {
 		event.preventDefault();
