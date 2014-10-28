d569b49cc2e7423ebf4dbc6a52b4dbb5694ccfa2
Mirko Pagliai <mirko.pagliai@gmail.com>
2014-10-28 19:17:39 +0100

    fixed

--- Controller/MeCmsAppController.php
+++ Controller/MeCmsAppController.php
@@ -22,6 +22,7 @@
  * @license		http://www.gnu.org/licenses/agpl.txt AGPL License
  * @link		http://git.novatlantis.it Nova Atlantis Ltd
  * @package		MeCms\Controller\Component
+ */
 
 App::uses('AppController', 'Controller');
 
