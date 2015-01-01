90f039e5fd5976082168250bcbcc73b01b44831f
Mirko Pagliai <mirko.pagliai@gmail.com>
2015-01-01 20:02:31 +0100

    updated copyright year

--- View/Helper/MenuHelper.php
+++ View/Helper/MenuHelper.php
@@ -18,7 +18,7 @@
  * along with MeCms.  If not, see <http://www.gnu.org/licenses/>.
  *
  * @author		Mirko Pagliai <mirko.pagliai@gmail.com>
- * @copyright	Copyright (c) 2014, Mirko Pagliai for Nova Atlantis Ltd
+ * @copyright	Copyright (c) 2015, Mirko Pagliai for Nova Atlantis Ltd
  * @license		http://www.gnu.org/licenses/agpl.txt AGPL License
  * @link		http://git.novatlantis.it Nova Atlantis Ltd
  * @package		MeCms\View\Helper
--- webroot/css/frontend/layout.css
+++ webroot/css/frontend/layout.css
@@ -1,7 +1,7 @@
 /*!
  * This file is part of MeCms.
  * @author		Mirko Pagliai <mirko.pagliai@gmail.com>
- * @copyright	Copyright (c) 2014, Mirko Pagliai for Nova Atlantis Ltd
+ * @copyright	Copyright (c) 2015, Mirko Pagliai for Nova Atlantis Ltd
  * @license		http://www.gnu.org/licenses/agpl.txt AGPL License
  */
 
