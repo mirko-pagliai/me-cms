973e22f637502c160056b0adbce24b39407c3baa
Mirko Pagliai <mirko.pagliai@gmail.com>
2015-03-12 11:07:43 +0100

    fixed

--- Config/widgets_map.php
+++ Config/widgets_map.php
@@ -3,5 +3,5 @@ $config = array('WidgetsMap' => array(
 	'MeCms.categories'		=> 'MeCms.PostsWidget::categories',
 	'MeCms.latest_posts'	=> 'MeCms.PostsWidget::latest',
 	'MeCms.pages'			=> 'MeCms.PagesWidget::pages',
-	'MeCms.random_photo'	=> 'MeCms.PhotosWidget::random',
+	'MeCms.random_photo'	=> 'MeCms.PhotosWidget::random'
 ));
