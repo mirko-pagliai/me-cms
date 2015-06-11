42ddb82f82e8722f3821dca5d8c43cb7e1e93f1f
Mirko Pagliai <mirko.pagliai@gmail.com>
2015-06-11 18:44:08 +0200

    added installer

---
+++ src/Console/Installer.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * This file is part of MeCms.
+ *
+ * MeCms is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * MeCms is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with MeCms.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author		Mirko Pagliai <mirko.pagliai@gmail.com>
+ * @copyright	Copyright (c) 2015, Mirko Pagliai for Nova Atlantis Ltd
+ * @license		http://www.gnu.org/licenses/agpl.txt AGPL License
+ * @link		http://git.novatlantis.it Nova Atlantis Ltd
+ */
+namespace MeCms\Console;
+
+use MeTools\Console\Installer as BaseInstaller;
+use Composer\Script\Event;
+use Exception;
+
+/**
+ * Provides installation hooks for when this application is installed via
+ * composer. Customize this class to suit your needs.
+ */
+class Installer extends BaseInstaller {
+    /**
+     * Does some routine installation tasks so people don't have to
+     * @param \Composer\Script\Event $event The composer event object
+	 * @uses MeTools\Console\Installer::postInstall()
+	 */
+    public static function postInstall(Event $event) {
+		parent::postInstall($event);
+    }
+}
