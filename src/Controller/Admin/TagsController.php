78e2d3796ff58358a3535604e513e6f78adf2a46
Mirko Pagliai <mirko.pagliai@gmail.com>
2015-11-21 20:48:22 +0100

    added "isAuthorized()" method for tags controller

--- src/Controller/Admin/TagsController.php
+++ src/Controller/Admin/TagsController.php
@@ -30,6 +30,21 @@ use MeCms\Controller\AppController;
  */
 class TagsController extends AppController {
 	/**
+	 * Check if the provided user is authorized for the request
+	 * @param array $user The user to check the authorization of. If empty the user in the session will be used
+	 * @return bool TRUE if the user is authorized, otherwise FALSE
+	 * @uses MeCms\Controller\Component\AuthComponent::isGroup()
+	 * @uses MeTools\Network\Request::isAction()
+	 */
+	public function isAuthorized($user = NULL) {
+		//Only admins and managers can edit tags
+		if($this->request->isAction('edit'))
+			return $this->Auth->isGroup(['admin', 'manager']);
+		
+		return TRUE;
+	}
+	
+	/**
      * Edits tag
      * @param string $id Tag ID
 	 */
