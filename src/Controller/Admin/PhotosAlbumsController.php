dc50c599948a3405dded401beff1a29491485195
Mirko Pagliai <mirko.pagliai@gmail.com>
2015-11-21 20:51:13 +0100

    fixed descriptions

--- src/Controller/Admin/PhotosAlbumsController.php
+++ src/Controller/Admin/PhotosAlbumsController.php
@@ -52,7 +52,6 @@ class PhotosAlbumsController extends AppController {
 	 * Check if the provided user is authorized for the request
 	 * @param array $user The user to check the authorization of. If empty the user in the session will be used
 	 * @return bool TRUE if the user is authorized, otherwise FALSE
-	 * @uses MeCms\Controller\AppController::isAuthorized()
 	 * @uses MeCms\Controller\Component\AuthComponent::isGroup()
 	 * @uses MeTools\Network\Request::isAction()
 	 */
--- src/Controller/Admin/PostsController.php
+++ src/Controller/Admin/PostsController.php
@@ -86,7 +86,6 @@ class PostsController extends AppController {
 	 * Check if the provided user is authorized for the request
 	 * @param array $user The user to check the authorization of. If empty the user in the session will be used
 	 * @return bool TRUE if the user is authorized, otherwise FALSE
-	 * @uses MeCms\Controller\AppController::isAuthorized()
 	 * @uses MeCms\Controller\Component\AuthComponent::isGroup()
 	 * @uses MeCms\Model\Table\AppTable::isOwnedBy()
 	 * @uses MeTools\Network\Request::isAction()
