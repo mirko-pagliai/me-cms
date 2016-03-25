c6f4079fe8aba53f53d2c42253f34ef7303e822d
Mirko Pagliai <mirko.pagliai@gmail.com>
2016-03-25 18:25:56 +0100

    fixed code

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -24,6 +24,7 @@
 use Cake\Cache\Cache;
 use Cake\Core\Configure;
 use Cake\Core\Plugin;
+use MeTools\Network\Request;
 
 /**
  * (here `\Cake\Core\Plugin` is used, as the plugins are not yet all loaded)
@@ -103,6 +104,6 @@ Configure::write('MeCms.frontend.widgets', Configure::consume('Widgets'));
 /**
  * Adds `isAdmin()` detector for the request
  */
-\MeTools\Network\Request::addDetector('admin', function ($request) {
+Request::addDetector('admin', function ($request) {
     return $request->param('prefix') === 'admin';
 });
