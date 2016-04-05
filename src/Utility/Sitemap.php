9933dd0b04c707e80c3e06d45fc305eb5dceded2
Mirko Pagliai <mirko.pagliai@gmail.com>
2016-04-05 17:18:17 +0200

    finished classes for sitemap

--- src/Utility/Sitemap.php
+++ src/Utility/Sitemap.php
@@ -19,61 +19,65 @@
  * @copyright	Copyright (c) 2016, Mirko Pagliai for Nova Atlantis Ltd
  * @license		http://www.gnu.org/licenses/agpl.txt AGPL License
  * @link		http://git.novatlantis.it Nova Atlantis Ltd
+ * @see         MeCms\Utility\SitemapBuilder
  */
 namespace MeCms\Utility;
 
-use Cake\Routing\Router;
 use Cake\ORM\TableRegistry;
-use Cake\Utility\Xml;
+use MeCms\Utility\SitemapBuilder;
 
-class Sitemap {    
-    protected function _pages() {
-        $url = [];
-        
+/**
+ * This class contains methods called by the `SitemapBuilder`.
+ * Each method must be return an array or urls to add to the sitemap.
+ */
+class Sitemap extends SitemapBuilder {
+    /**
+     * Method that returns pages urls
+     * @return array
+     * @uses MeCms\Utility\SitemapBuilder::url()
+     */
+    public static function pages() {
         $pages = TableRegistry::get('MeCms.Pages')->find('active')->select(['slug']);
         
-        if(!$pages->isEmpty()) {
-            //Pages index
-            $url[] = self::url(['_name' => 'pages']);
-
-            //All pages
-            $url = am($url, array_map(function($page) {
-                return self::url(['_name' => 'page', $page->slug]);
-            }, $pages->toArray()));
+        if($pages->isEmpty()) {
+            return [];
         }
         
-        $statics = \MeCms\Utility\StaticPage::all();
-        
-        //All static pages
+        //Adds pages index
+        $url = [self::url(['_name' => 'pages'])];
+
+        //Adds all pages
         $url = am($url, array_map(function($page) {
             return self::url(['_name' => 'page', $page->slug]);
-        }, $statics));
+        }, $pages->toArray()));
         
         return $url;
     }
 
-
-    protected function _photos() {
-        $url = [];
-        
+    /**
+     * Method that returns photos urls
+     * @return array
+     * @uses MeCms\Utility\SitemapBuilder::url()
+     */
+    public static function photos() {
         $albums = TableRegistry::get('MeCms.PhotosAlbums')->find('active')
             ->select(['id', 'slug'])
             ->contain(['Photos' => function($q) {
                 return $q->select(['id', 'album_id']);
             }]);
         
-        //No albums, no photos. Return
-        if($albums->isEmpty())
+        if($albums->isEmpty()) {
             return [];
+        }
         
-        //Albums index
-        $url[] = self::url(['_name' => 'albums']);
+        //Adds albums index
+        $url = [self::url(['_name' => 'albums'])];
 
         foreach($albums->toArray() as $album) {
-            //All albums
+            //Adds the album
             $url[] = self::url(['_name' => 'album', $album->slug]);
 
-            //For each album, all photos
+            //Adds the photos
             $url = am($url, array_map(function($photo) {
                 return self::url(['_name' => 'photo', $photo->id]);
             }, $album->photos));
@@ -82,57 +86,90 @@ class Sitemap {
         return $url;
     }
     
-    protected function _posts() {
-        $table = TableRegistry::get('MeCms.PostsCategories');
-        $url = [];
-        
-        $categories = $table->find('active')
+    /**
+     * Method that returns posts urls
+     * @return array
+     * @uses MeCms\Utility\SitemapBuilder::url()
+     */
+    public static function posts() {        
+        $categories = TableRegistry::get('MeCms.PostsCategories')->find('active')
             ->select(['id', 'slug'])
             ->contain(['Posts' => function($q) {
                 return $q->select(['category_id', 'slug']);
             }]);
         
-        //No categories, no posts. Return
-        if($categories->isEmpty())
+        if($categories->isEmpty()) {
             return [];
+        }
         
-        //Posts index
-        $url[] = self::url(['_name' => 'posts']);
-        
-        //Categories index
-        $url[] = self::url(['_name' => 'posts_categories']);
+        //Adds posts index, categories index and posts search
+        $url = [
+            self::url(['_name' => 'posts']),
+            self::url(['_name' => 'posts_categories']),
+            self::url(['_name' => 'search_posts']),
+        ];
         
         foreach($categories as $category) {
-            //All categories
+            //Adds the category
             $url[] = self::url(['_name' => 'posts_category', $category->slug]);
             
-            //For each category, all posts
+            //Adds the posts
             $url = am($url, array_map(function($post) {
                 return self::url(['_name' => 'post', $post->slug]);
             }, $category->posts));
         }
         
-        //Posts search
-        $url[] = self::url(['_name' => 'search_posts']);
-        
-        $tags = $table->Posts->Tags->find('all')
+        return $url;
+    }
+    
+    /**
+     * Method that returns posts tags urls
+     * @return array
+     * @uses MeCms\Utility\SitemapBuilder::url()
+     */
+    public static function posts_tags() {            
+        $tags = TableRegistry::get('MeCms.Tags')->find('all')
 			->order(['tag' => 'ASC'])
 			->where(['post_count >' => 0]);
         
-        if(!$tags->isEmpty()) {
-            //Tags index
-            $url[] = self::url(['_name' => 'posts_tags']);
-            
-            //All tags
-            $url = am($url, array_map(function($tag) {
-                return self::url(['_name' => 'posts_tag', $tag->slug]);
-            }, $tags->toArray()));
+        if($tags->isEmpty()) {
+            return [];
         }
+        
+        //Adds the tags index
+        $url[] = self::url(['_name' => 'posts_tags']);
+
+        //Adds all tags
+        $url = am($url, array_map(function($tag) {
+            return self::url(['_name' => 'posts_tag', $tag->slug]);
+        }, $tags->toArray()));
         
         return $url;
     }
     
-    protected function _system() {
+    /**
+     * Method that returns static pages urls
+     * @return array
+     * @uses MeCms\Utility\SitemapBuilder::url()
+     * @uses MeCms\Utility\StaticPage::all()
+     */
+    public static function static_pages() {
+        $statics = \MeCms\Utility\StaticPage::all();
+        
+        //Adds static pages
+        $url = array_map(function($page) {
+            return self::url(['_name' => 'page', $page->slug]);
+        }, $statics);
+        
+        return $url;
+    }
+    
+    /**
+     * Method that returns systems urls.
+     * @return array
+     * @uses MeCms\Utility\SitemapBuilder::url()
+     */
+    public static function systems() {
         $url = [];
         
         //Contact form
@@ -140,26 +177,5 @@ class Sitemap {
             $url[] = self::url(['_name' => 'contact_form']);
         
         return $url;
-    }
-
-    protected function url($url) {
-        return Router::url($url, TRUE);
-    }
-
-    public function generate() {
-        //Home page
-        $url = [self::url('/')];
-        
-        $url = am($url, self::_pages(), self::_photos(), self::_posts(), self::_system());
-        
-        $url = array_map(function($url) {
-            return ['loc' => $url];
-        }, $url);
-                
-        $xml = Xml::fromArray(['urlset' => am(compact('url'), ['xmlns:' => 'http://www.sitemaps.org/schemas/sitemap/0.9'])], ['pretty' => TRUE]);
-        
-        debug($xml->asXML());
-        
-        exit;
     }
 }
---
+++ src/Utility/SitemapBuilder.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * This file is part of MeCms.
+ *
+ * MeCms is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * MeCms is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with MeCms.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author		Mirko Pagliai <mirko.pagliai@gmail.com>
+ * @copyright	Copyright (c) 2016, Mirko Pagliai for Nova Atlantis Ltd
+ * @license		http://www.gnu.org/licenses/agpl.txt AGPL License
+ * @link		http://git.novatlantis.it Nova Atlantis Ltd
+ */
+namespace MeCms\Utility;
+
+use Cake\Utility\Xml;
+use MeTools\Core\Plugin;
+
+/**
+ * An utility to generate a sitemap
+ */
+class SitemapBuilder {
+    /**
+     * Internal method to generate url for the sitempa
+     * @param string|array|null $url
+     * @return string
+     * @see Cake\Routing\Router::url()
+     */
+    protected static function url($url) {
+        return \Cake\Routing\Router::url($url, TRUE);
+    }
+
+    /**
+     * Generate the sitemap.
+     * 
+     * For each plugin, it calls dynamically all methods from class 
+     * `$PLUGIN\Utility\Sitemap`.
+     * Each method must be return an array or urls to add to the sitemap.
+     * @see MeCms\Utility\Sitemap
+     * @uses MeTools\Core\Plugin::all()
+     * @uses url()
+     */
+    public function generate() {
+        //Adds the homepage
+        $url = [self::url('/')];
+        
+        foreach(am(['MeCms'], Plugin::all(['DebugKit', 'MeCms', 'MeTools', 'Migrations'])) as $plugin) {
+            //Sets the class name
+            $class = sprintf('\%s\Utility\Sitemap', $plugin);
+            
+            //Gets all methods from `$PLUGIN\Utility\Sitemap` class
+            $methods = get_class_methods($class);
+            
+            if(empty($methods)) {
+                continue;
+            }
+            
+            //Because each `Utility` class may be an extension of this class, 
+            //it calculates the difference between the methods of the two classes
+            $methods = array_diff($methods, get_class_methods(__CLASS__));
+            
+            //Calls all methods 
+            foreach($methods as $method) {
+                $url = am($url, call_user_func([$class, $method]));
+            }
+        }
+                
+        $xml = Xml::fromArray(['urlset' => [
+            'xmlns:' => 'http://www.sitemaps.org/schemas/sitemap/0.9',
+            'url' => array_map(function($url) { return ['loc' => $url]; }, $url),
+        ]], ['pretty' => TRUE]);
+        
+        return $xml->asXML();
+    }
+}
