31987cdaec23c57db75a6b2d90a3f803c0565abb
mirko-pagliai <mirko.pagliai@gmail.com>
2017-03-23 21:26:34 +0100

    added descriptions

--- src/TestSuite/Traits/AuthMethodsTrait.php
+++ src/TestSuite/Traits/AuthMethodsTrait.php
@@ -22,8 +22,22 @@
  */
 namespace MeCms\TestSuite\Traits;
 
+/**
+ * This trait provides some useful methods to test authentication within the
+ *  controllers.
+ *
+ * It is necessary that the current controller is contained in the `$Controller`
+ *  property of the test class.
+ */
 trait AuthMethodsTrait
 {
+    /**
+     * Asserts that groups are authorized
+     * @param array $values Group name as key and boolean as value
+     * @return void
+     * @uses $Controller
+     * @uses setUserGroup()
+     */
     public function assertGroupsAreAuthorized($values)
     {
         foreach ($values as $group => $isAllowed) {
@@ -32,15 +46,27 @@ trait AuthMethodsTrait
         }
     }
 
+    /**
+     * Asserts that users are authorized
+     * @param array $values UserID as key and boolean as value
+     * @return void
+     * @uses $Controller
+     * @uses setUserId()
+     */
     public function assertUsersAreAuthorized($values)
     {
         foreach ($values as $id => $isAllowed) {
             $this->setUserId($id);
             $this->assertEquals($isAllowed, $this->Controller->isAuthorized());
         }
-
     }
 
+    /**
+     * Internal method to set the user group
+     * @param int $id User ID
+     * @return void
+     * @uses $Controller
+     */
     protected function setUserId($id)
     {
         $this->Controller->Auth->setUser(['id' => $id]);
@@ -50,6 +76,8 @@ trait AuthMethodsTrait
     /**
      * Internal method to set the user group
      * @param string $group Group name
+     * @return void
+     * @uses $Controller
      */
     protected function setUserGroup($group)
     {
