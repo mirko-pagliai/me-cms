d20a9bd5c3b441c098ecb82a4e0a2e1df46fe7df
mirko-pagliai <mirko.pagliai@gmail.com>
2018-06-18 13:19:34 +0200

    fixed

--- src/Shell/InstallShell.php
+++ src/Shell/InstallShell.php
@@ -62,11 +62,11 @@ class InstallShell extends BaseInstallShell
         ];
 
         //Merges assets for which create symbolic links
-        $this->links += [
+        $this->links = array_merge($this->links, [
             'js-cookie' . DS . 'js-cookie' . DS . 'src' => 'js-cookie',
             'sunhater' . DS . 'kcfinder' => 'kcfinder',
             'enyo' . DS . 'dropzone' . DS . 'dist' => 'dropzone',
-        ];
+        ]);
 
         //Merges paths to be created and made writable
         $this->paths = array_merge($this->paths, [
