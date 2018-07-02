83a2dd1864f5bc99265ef4a087d451464ca51f44
mirko-pagliai <mirko.pagliai@gmail.com>
2018-07-02 17:44:02 +0200

    fixed bug

--- src/Model/Entity/User.php
+++ src/Model/Entity/User.php
@@ -55,10 +55,14 @@ class User extends Entity
 
     /**
      * Gets the full name (virtual field)
-     * @return string
+     * @return string|void
      */
     protected function _getFullName()
     {
+        if (empty($this->_properties['first_name']) || empty($this->_properties['last_name'])) {
+            return;
+        }
+
         return sprintf('%s %s', $this->_properties['first_name'], $this->_properties['last_name']);
     }
 
