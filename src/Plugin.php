14368460862a1a0f733ada36b35c77974e5f325a
mirko-pagliai <mirko.pagliai@gmail.com>
2018-11-20 09:34:30 +0100

    fixed for sniffer

--- src/Plugin.php
+++ src/Plugin.php
@@ -29,7 +29,7 @@ class Plugin extends BasePlugin
 {
     /**
      * Load all the application configuration and bootstrap logic
-     * @param PluginApplicationInterface $app
+     * @param PluginApplicationInterface $app The host application
      * @return void
      */
     public function bootstrap(PluginApplicationInterface $app)
