2069ff4f748cc47c46ce38d8a64b5bf69b9a060c
mirko-pagliai <mirko.pagliai@gmail.com>
2019-07-10 12:15:25 +0200

    fixed

--- src/View/Helper/MenuBuilderHelper.php
+++ src/View/Helper/MenuBuilderHelper.php
@@ -61,7 +61,7 @@ class MenuBuilderHelper extends Helper
     /**
      * Generates all menus for a plugin
      * @param string $plugin Plugin name
-     * @return string|null Html code
+     * @return array Menus
      * @uses getMenuMethods()
      */
     public function generate($plugin)
