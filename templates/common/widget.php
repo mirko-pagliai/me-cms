363e81bc88cdfab48541e6896bc7c99ec4e43e22
mirko-pagliai <mirko.pagliai@gmail.com>
2020-02-12 14:39:13 +0100

    fixed for sniffer

--- templates/common/widget.php
+++ templates/common/widget.php
@@ -18,7 +18,7 @@ $class = trim($this->fetch('class')) ? sprintf('widget %s', trim($this->fetch('c
 
 <?php if (trim($this->fetch('content'))) : ?>
 <div class="<?= $class ?> mb-4">
-    <?php if(trim($this->fetch('title'))): ?>
+    <?php if(trim($this->fetch('title'))) : ?>
     <h4 class="widget-title"><?= trim($this->fetch('title')) ?></h4>
     <?php endif; ?>
 
