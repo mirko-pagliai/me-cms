1ea5738dd9f2dd3508dcc1900a6a4aab883cafab
mirko-pagliai <mirko.pagliai@gmail.com>
2021-01-23 12:52:37 +0100

    fixed little bug

--- src/Core/Plugin.php
+++ src/Core/Plugin.php
@@ -40,7 +40,7 @@ class Plugin extends BasePlugin
         $options += ['exclude' => [], 'order' => true, 'mecms_core' => true];
 
         if (!$options['mecms_core']) {
-            $options['exclude'] = array_merge((array)$options['exclude'], ['MeTools', 'Assets', 'DatabaseBackup', 'Recaptcha', 'RecaptchaMailhide', 'StopSpam', 'Thumber/Cake', 'Tokens']);
+            $options['exclude'] = array_merge((array)$options['exclude'], ['MeTools', 'Assets', 'DatabaseBackup', 'Recaptcha', 'RecaptchaMailhide', 'StopSpam', 'Thumber/Cake', 'Tokens', 'WyriHaximus/MinifyHtml']);
         }
 
         $plugins = parent::all($options);
