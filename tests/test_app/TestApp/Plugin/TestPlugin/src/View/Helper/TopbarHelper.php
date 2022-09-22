21285a4c3ff1e7624d166775f250f12db1f4b22f
mirko-pagliai <mirko.pagliai@gmail.com>
2022-09-22 18:40:43 +0200

    fixed for `BootstrapHtmlHelper`

--- tests/test_app/TestApp/Plugin/TestPlugin/src/View/Helper/TopbarHelper.php
+++ tests/test_app/TestApp/Plugin/TestPlugin/src/View/Helper/TopbarHelper.php
@@ -3,18 +3,12 @@ declare(strict_types=1);
 
 namespace TestPlugin\View\Helper;
 
-use Cake\View\Helper;
+use MeCms\View\Helper\TopbarHelper as BaseTopbarHelper;
 
-class TopbarHelper extends Helper
+class TopbarHelper extends BaseTopbarHelper
 {
-    public $helpers = [
-        'Html' => ['className' => 'MeTools.Html'],
-    ];
-
     public function build(): array
     {
-        return [
-            $this->Html->link('A link from plugin', 'http://localhost'),
-        ];
+        return [$this->Html->link('A link from plugin', 'http://localhost')];
     }
 }
