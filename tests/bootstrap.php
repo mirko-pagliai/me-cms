c5b9041d2c4951379ca13c48c15108323dc97f76
Mirko Pagliai <mirko.pagliai@gmail.com>
2022-10-07 09:42:51 +0200

    removed silence operator

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -50,7 +50,9 @@ foreach ([
     CACHE . 'persistent',
     CACHE . 'views',
 ] as $dir) {
-    @mkdir($dir, 0777, true);
+    if (!file_exists($dir)) {
+        mkdir($dir, 0777, true);
+    }
 }
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
