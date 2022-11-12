18458062f2bab14524d804e1b6640c37eedf22ca
Mirko Pagliai <mirko.pagliai@gmail.com>
2022-11-12 16:29:49 +0100

    fixed for postgres

--- tests/schema.sql
+++ tests/schema.sql
@@ -1,8 +1,8 @@
-CREATE TABLE IF NOT EXISTS `articles` (
-    `id` INTEGER AUTO_INCREMENT,
-    `author_id` INTEGER,
-    `title` VARCHAR(255),
-    `body` TEXT,
-    `published` VARCHAR(1) DEFAULT 'N',
-    PRIMARY KEY (`id`)
+CREATE TABLE IF NOT EXISTS articles (
+    id INTEGER AUTO_INCREMENT,
+    author_id INTEGER,
+    title VARCHAR(255),
+    body TEXT,
+    published VARCHAR(1) DEFAULT 'N',
+    PRIMARY KEY (id)
 )
