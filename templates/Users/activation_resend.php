a768bcff1f76ce5c3f1da300ff87f004c4ce6e6a
mirko-pagliai <mirko.pagliai@gmail.com>
2023-04-04 17:17:17 +0200

    little fixes

--- templates/Users/activation_resend.php
+++ templates/Users/activation_resend.php
@@ -11,6 +11,9 @@ declare(strict_types=1);
  * @copyright   Copyright (c) Mirko Pagliai
  * @link        https://github.com/mirko-pagliai/me-cms
  * @license     https://opensource.org/licenses/mit-license.php MIT License
+ *
+ * @var \MeCms\View\View\AppView $this
+ * @var \MeCms\Model\Entity\User $user
  */
 $this->extend('MeCms./common/form');
 $this->assign('title', $title = __d('me_cms', 'Resend activation email'));
